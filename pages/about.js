0dcd255bce64b061aaa9d4014d7b8bb9f3f231b6
Jisan-mia <jisanmia47@gmail.com>
2021-08-01 22:02:37 +0600

    intialize nextjs app

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptate assumenda, maiores deserunt sit totam a maxime, aperiam tempore ducimus dolor repellat praesentium autem sunt alias quaerat nostrum reiciendis amet?</p>
+            
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias aliquam quae aliquid optio accusantium accusamus possimus quidem illo porro provident libero voluptates, eveniet fuga, magni dicta rerum dolores commodi mollitia!</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Ninja() {
+    return (
+        <div>
+            <h1>The Ninjas Pages</h1>
+        
+        </div>
+    )
+}
+
+export default Ninja
